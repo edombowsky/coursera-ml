4acb0d4cea104b48711674d1e15e6c6e815aa7a9
edombowsky <edombowsky@gmail.com>
2016-05-29 17:55:43 -0700

    remove some temporary code (it did not work)

--- NeuralNetworks/ex3/lrCostFunction.m
+++ NeuralNetworks/ex3/lrCostFunction.m
@@ -36,19 +36,6 @@ grad = zeros(size(theta));
 %           grad = grad + YOUR_CODE_HERE (using the temp variable)
 %
 
-
-% J = 1./m * (-y' * log(sigmoid(X * theta)) - (1 - y') * log(1 - sigmoid(X * theta)))
-% grad = 1./m * X' * (sigmoid(X * theta) - y)
-
-
-% J = ( (1 / m) * sum(-y'*log(sigmoid(X*theta)) - (1-y)'*log( 1 - sigmoid(X*theta))) ) + (lambda/(2*m))*sum(theta(2:length(theta)).*theta(2:length(theta))) ;
-
-% grad = (1 / m) * sum( X .* repmat((sigmoid(X*theta) - y), 1, size(X,2)) );
-
-% grad(:,2:length(grad)) = grad(:,2:length(grad)) + (lambda/m)*theta(2:length(theta))';
-
-
-
 J = 1/m * sum(-y .* log(sigmoid(X*theta)) - (1 - y) .* log(1 - sigmoid(X*theta))) ...
   + lambda / (2*m) * sum(theta(2:end) .^ 2);
 
